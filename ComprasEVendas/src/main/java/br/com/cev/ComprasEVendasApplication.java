503d22e1a0dec62e82e70c76192a80cbd19ea71d
lucas.campos <lucas.campos@docket.com.br>
2024-10-18 14:45:52 -0300

    Projeto CEV com conexão no banco da aws

---
+++ ComprasEVendas/src/main/java/br/com/cev/ComprasEVendasApplication.java
@@ -0,0 +1,13 @@
+package br.com.cev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComprasEVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComprasEVendasApplication.class, args);
+	}
+
+}
